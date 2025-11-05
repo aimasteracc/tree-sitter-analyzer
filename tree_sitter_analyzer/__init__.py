f7d90d6aa227bf568a2453bfc2e5ebaff5e99d33
aisheng.yu <yuaishengtrader@gmail.com>
2025-11-05 10:45:28 +0900

    Release v1.9.4: GitFlow automated release - version update and documentation sync

--- tree_sitter_analyzer/__init__.py
+++ tree_sitter_analyzer/__init__.py
@@ -11,7 +11,7 @@ Architecture:
 - Data Models: Generic and language-specific code element representations
 """
 
-__version__ = "1.9.3"
+__version__ = "1.9.4"
 __author__ = "aisheng.yu"
 __email__ = "aimasteracc@gmail.com"
 
