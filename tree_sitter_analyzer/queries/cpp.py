ef589efe4668eb72198e3e3c88f22f26870604ed
Mr. Rc <60568652+HACKE-RC@users.noreply.github.com>
2025-11-27 15:48:51 +0530

    Merge pull request #1 from rcx86/copilot/add-advanced-analysis-support
    Add support for C/C++ advanced analysis

--- tree_sitter_analyzer/queries/cpp.py
+++ tree_sitter_analyzer/queries/cpp.py
@@ -3,30 +3,25 @@
 C++ Language Queries
 
 Tree-sitter queries specific to C++ language constructs.
-Covers classes, functions, methods, namespaces, templates, and other C++-specific elements.
+Covers classes, functions, templates, namespaces, and other C++ specific elements.
 """
 
 # C++-specific query library
 CPP_QUERIES: dict[str, str] = {
-    # --- Preprocessor ---
-    "include": """
-    (preproc_include) @include
+    # --- Basic Structure ---
+    "class": """
+    (class_specifier) @class
     """,
-    "define": """
-    (preproc_def) @define
+    "struct": """
+    (struct_specifier) @struct
     """,
-    # --- Namespaces ---
-    "namespace": """
-    (namespace_definition) @namespace
+    "union": """
+    (union_specifier) @union
     """,
-    "using": """
-    (using_declaration) @using
+    "enum": """
+    (enum_specifier) @enum
     """,
-    "using_directive": """
-    (using_declaration
-      (identifier)) @using_directive
-    """,
-    # --- Functions ---
+    # --- Functions and Methods ---
     "function": """
     (function_definition) @function
     """,
@@ -34,14 +29,25 @@ CPP_QUERIES: dict[str, str] = {
     (declaration
       declarator: (function_declarator)) @function_declaration
     """,
-    # --- Classes and Structs ---
-    "class": """
-    (class_specifier
-      body: (field_declaration_list)) @class
+    "method": """
+    (function_definition
+      declarator: (function_declarator
+        declarator: (field_identifier) @method.name)) @method
     """,
-    "struct": """
-    (struct_specifier
-      body: (field_declaration_list)) @struct
+    "constructor": """
+    (function_definition
+      declarator: (function_declarator
+        declarator: (identifier) @ctor.name)
+      body: (compound_statement)) @constructor
+    """,
+    "destructor": """
+    (function_definition
+      declarator: (function_declarator
+        declarator: (destructor_name) @dtor.name)) @destructor
+    """,
+    "virtual_function": """
+    (function_definition
+      (virtual)) @virtual_function
     """,
     # --- Templates ---
     "template": """
@@ -49,116 +55,202 @@ CPP_QUERIES: dict[str, str] = {
     """,
     "template_function": """
     (template_declaration
-      (function_definition)) @template_function
+      (function_definition) @template_function)
     """,
     "template_class": """
     (template_declaration
-      (class_specifier)) @template_class
+      (class_specifier) @template_class)
     """,
-    # --- Access Specifiers ---
-    "public": """
-    (access_specifier) @public
+    "template_parameter": """
+    (template_parameter_list
+      (type_parameter_declaration) @template_parameter)
     """,
-    # --- Variables ---
-    "global_var": """
-    (translation_unit
-      (declaration) @global_var)
-    """,
-    "field": """
-    (field_declaration) @field
-    """,
-    # --- Name-only Extraction ---
-    "function_name": """
-    (function_definition
-      declarator: (function_declarator
-        declarator: (identifier) @function_name))
-    """,
-    "class_name": """
-    (class_specifier
-      name: (type_identifier) @class_name)
-    """,
-    "struct_name": """
-    (struct_specifier
-      name: (type_identifier) @struct_name)
+    # --- Namespaces ---
+    "namespace": """
+    (namespace_definition) @namespace
     """,
     "namespace_name": """
     (namespace_definition
       name: (identifier) @namespace_name)
     """,
-    # --- Detailed Queries ---
-    "function_with_params": """
-    (function_definition
-      declarator: (function_declarator
-        declarator: (identifier) @name
-        parameters: (parameter_list) @params)
-      body: (compound_statement) @body) @function_with_params
+    "using_declaration": """
+    (using_declaration) @using_declaration
     """,
-    "class_with_members": """
-    (class_specifier
-      name: (type_identifier) @name
-      body: (field_declaration_list) @members) @class_with_members
+    "using_directive": """
+    (using_directive) @using_directive
     """,
-    "method_definition": """
-    (function_definition
-      declarator: (function_declarator
-        declarator: (field_identifier) @method_name)) @method_definition
+    # --- Includes and Preprocessor ---
+    "include": """
+    (preproc_include) @include
     """,
-    # --- Control Flow ---
-    "if": """
-    (if_statement) @if
+    "system_include": """
+    (preproc_include
+      path: (system_lib_string) @system_include)
     """,
-    "for": """
-    (for_statement) @for
+    "local_include": """
+    (preproc_include
+      path: (string_literal) @local_include)
+    """,
+    "define": """
+    (preproc_def) @define
+    """,
+    "ifdef": """
+    (preproc_ifdef) @ifdef
+    """,
+    "macro_function": """
+    (preproc_function_def) @macro_function
+    """,
+    # --- Variables and Fields ---
+    "field": """
+    (field_declaration) @field
+    """,
+    "variable": """
+    (declaration) @variable
+    """,
+    "static_variable": """
+    (declaration
+      (storage_class_specifier) @storage
+      (#eq? @storage "static")) @static_variable
+    """,
+    "const_variable": """
+    (declaration
+      (type_qualifier) @qualifier
+      (#eq? @qualifier "const")) @const_variable
+    """,
+    # --- Access Specifiers ---
+    "public_access": """
+    (access_specifier) @public_access
+    (#eq? @public_access "public:")
+    """,
+    "private_access": """
+    (access_specifier) @private_access
+    (#eq? @private_access "private:")
+    """,
+    "protected_access": """
+    (access_specifier) @protected_access
+    (#eq? @protected_access "protected:")
+    """,
+    # --- Inheritance ---
+    "base_class": """
+    (base_class_clause
+      (base_specifier) @base_class)
+    """,
+    "public_inheritance": """
+    (base_class_clause
+      (base_specifier
+        (access_specifier) @access
+        (#eq? @access "public")
+        (type_identifier) @base_type)) @public_inheritance
+    """,
+    # --- Smart Pointers and Modern C++ ---
+    "smart_pointer": """
+    (template_type
+      name: (type_identifier) @ptr_type
+      (#match? @ptr_type "^(unique_ptr|shared_ptr|weak_ptr)$")) @smart_pointer
+    """,
+    "auto_type": """
+    (declaration
+      type: (auto)) @auto_type
+    """,
+    "lambda": """
+    (lambda_expression) @lambda
     """,
     "range_for": """
     (for_range_loop) @range_for
     """,
-    "while": """
-    (while_statement) @while
-    """,
-    "switch": """
-    (switch_statement) @switch
-    """,
+    # --- Exception Handling ---
     "try_catch": """
     (try_statement) @try_catch
+    """,
+    "catch_clause": """
+    (catch_clause) @catch_clause
+    """,
+    "throw_statement": """
+    (throw_statement) @throw_statement
+    """,
+    # --- Name-only Extraction ---
+    "class_name": """
+    (class_specifier
+      name: (type_identifier) @class_name)
+    """,
+    "function_name": """
+    (function_definition
+      declarator: (function_declarator
+        declarator: (identifier) @function_name))
+    """,
+    "field_name": """
+    (field_declaration
+      declarator: (field_identifier) @field_name)
     """,
     # --- Comments ---
     "comment": """
     (comment) @comment
     """,
+    "block_comment": """
+    (comment) @block_comment
+    (#match? @block_comment "^/\\*")
+    """,
+    # --- Operator Overloading ---
+    "operator_overload": """
+    (function_definition
+      declarator: (function_declarator
+        declarator: (operator_name) @operator_name)) @operator_overload
+    """,
+    # --- Friend Declaration ---
+    "friend": """
+    (friend_declaration) @friend
+    """,
 }
 
 # Query descriptions
 CPP_QUERY_DESCRIPTIONS: dict[str, str] = {
-    "include": "Extract #include preprocessor directives",
-    "define": "Extract #define preprocessor macros",
-    "namespace": "Extract namespace definitions",
-    "using": "Extract using declarations",
-    "using_directive": "Extract using directives",
+    "class": "Extract C++ class declarations",
+    "struct": "Extract C++ struct declarations",
+    "union": "Extract C++ union declarations",
+    "enum": "Extract C++ enum declarations",
     "function": "Extract C++ function definitions",
     "function_declaration": "Extract C++ function declarations",
-    "class": "Extract C++ class definitions",
-    "struct": "Extract C++ struct definitions",
-    "template": "Extract template declarations",
-    "template_function": "Extract template function declarations",
-    "template_class": "Extract template class declarations",
-    "public": "Extract access specifiers",
-    "global_var": "Extract global variable declarations",
-    "field": "Extract class/struct field declarations",
-    "function_name": "Extract function names only",
-    "class_name": "Extract class names only",
-    "struct_name": "Extract struct names only",
-    "namespace_name": "Extract namespace names only",
-    "function_with_params": "Extract function definitions with parameters",
-    "class_with_members": "Extract class definitions with members",
-    "method_definition": "Extract method definitions inside classes",
-    "if": "Extract if statements",
-    "for": "Extract for statements",
-    "range_for": "Extract range-based for loops",
-    "while": "Extract while statements",
-    "switch": "Extract switch statements",
-    "try_catch": "Extract try-catch blocks",
-    "comment": "Extract comments",
+    "method": "Extract C++ class methods",
+    "constructor": "Extract C++ constructors",
+    "destructor": "Extract C++ destructors",
+    "virtual_function": "Extract C++ virtual functions",
+    "template": "Extract C++ template declarations",
+    "template_function": "Extract C++ template functions",
+    "template_class": "Extract C++ template classes",
+    "template_parameter": "Extract C++ template parameters",
+    "namespace": "Extract C++ namespace definitions",
+    "namespace_name": "Extract C++ namespace names",
+    "using_declaration": "Extract C++ using declarations",
+    "using_directive": "Extract C++ using directives",
+    "include": "Extract C++ include directives",
+    "system_include": "Extract C++ system includes",
+    "local_include": "Extract C++ local includes",
+    "define": "Extract C++ preprocessor defines",
+    "ifdef": "Extract C++ preprocessor ifdef blocks",
+    "macro_function": "Extract C++ macro functions",
+    "field": "Extract C++ class/struct fields",
+    "variable": "Extract C++ variable declarations",
+    "static_variable": "Extract C++ static variables",
+    "const_variable": "Extract C++ const variables",
+    "public_access": "Extract C++ public access specifiers",
+    "private_access": "Extract C++ private access specifiers",
+    "protected_access": "Extract C++ protected access specifiers",
+    "base_class": "Extract C++ base class specifiers",
+    "public_inheritance": "Extract C++ public inheritance",
+    "smart_pointer": "Extract C++ smart pointer usage",
+    "auto_type": "Extract C++ auto type declarations",
+    "lambda": "Extract C++ lambda expressions",
+    "range_for": "Extract C++ range-based for loops",
+    "try_catch": "Extract C++ try-catch blocks",
+    "catch_clause": "Extract C++ catch clauses",
+    "throw_statement": "Extract C++ throw statements",
+    "class_name": "Extract C++ class names only",
+    "function_name": "Extract C++ function names only",
+    "field_name": "Extract C++ field names only",
+    "comment": "Extract C++ comments",
+    "block_comment": "Extract C++ block comments",
+    "operator_overload": "Extract C++ operator overloads",
+    "friend": "Extract C++ friend declarations",
 }
 
 
@@ -196,7 +288,7 @@ def get_cpp_query_description(name: str) -> str:
 
 
 # Convert to ALL_QUERIES format for dynamic loader compatibility
-ALL_QUERIES: dict[str, dict[str, str]] = {}
+ALL_QUERIES = {}
 for query_name, query_string in CPP_QUERIES.items():
     description = CPP_QUERY_DESCRIPTIONS.get(query_name, "No description")
     ALL_QUERIES[query_name] = {"query": query_string, "description": description}
@@ -208,23 +300,23 @@ ALL_QUERIES["functions"] = {
 }
 
 ALL_QUERIES["methods"] = {
-    "query": CPP_QUERIES["method_definition"],
-    "description": "Search all method definitions (alias for method_definition)",
+    "query": CPP_QUERIES["method"],
+    "description": "Search all method declarations (alias for method)",
 }
 
 ALL_QUERIES["classes"] = {
     "query": CPP_QUERIES["class"],
-    "description": "Search all class definitions (alias for class)",
+    "description": "Search all class declarations (alias for class)",
 }
 
-ALL_QUERIES["structs"] = {
-    "query": CPP_QUERIES["struct"],
-    "description": "Search all struct definitions (alias for struct)",
+ALL_QUERIES["imports"] = {
+    "query": CPP_QUERIES["include"],
+    "description": "Search all include directives (alias for include)",
 }
 
-ALL_QUERIES["namespaces"] = {
-    "query": CPP_QUERIES["namespace"],
-    "description": "Search all namespace definitions (alias for namespace)",
+ALL_QUERIES["variables"] = {
+    "query": CPP_QUERIES["variable"],
+    "description": "Search all variable declarations (alias for variable)",
 }
 
 
@@ -237,12 +329,12 @@ def get_query(name: str) -> str:
     )
 
 
-def get_all_queries() -> dict[str, dict[str, str]]:
+def get_all_queries() -> dict:
     """Get all available queries."""
     return ALL_QUERIES
 
 
-def list_queries() -> list[str]:
+def list_queries() -> list:
     """List all available query names."""
     return list(ALL_QUERIES.keys())
 
