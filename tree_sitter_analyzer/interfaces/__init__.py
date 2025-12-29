1e9b916eb0b97022e2066e886feeadb449a902b3
aisheng.yu <aimasteracc@gmail.com>
2025-12-29 15:30:57 +0900

    Remove interfaces module and related tests

--- tree_sitter_analyzer/interfaces/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-Interfaces Package
-
-This package contains the external interfaces for the tree-sitter analyzer.
-Each interface provides a different way to interact with the core analysis engine.
-"""
-
-# This file makes the interfaces directory a Python package
