434529d6d565edcf837374c7bc891bf6c226aa3a
aisheng.yu <aimasteracc@gmail.com>
2026-01-08 15:10:38 +0900

    Hotfix: Improve Vertex AI compatibility

--- tree_sitter_analyzer/__init__.py
+++ tree_sitter_analyzer/__init__.py
@@ -11,7 +11,7 @@ Architecture:
 - Data Models: Generic and language-specific code element representations
 """
 
-__version__ = "1.9.17"
+__version__ = "1.9.17.1"
 __author__ = "aisheng.yu"
 __email__ = "aimasteracc@gmail.com"
 
