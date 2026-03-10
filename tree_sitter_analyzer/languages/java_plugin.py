33cb5741457c64ef9895d9e52279727a69d9f1d1
aisheng.yu <yuaishengtrader@gmail.com>
2026-03-11 02:07:04 +0900

    fix(java): narrow create_extractor return type to JavaElementExtractor
    Fixes MyPy attr-defined errors for annotations, current_package, and
    imports attributes that exist on JavaElementExtractor but not on the
    base ElementExtractor class.

--- tree_sitter_analyzer/languages/java_plugin.py
+++ tree_sitter_analyzer/languages/java_plugin.py
@@ -1112,7 +1112,7 @@ class JavaPlugin(LanguagePlugin):
         """Get supported file extensions."""
         return [".java", ".jsp", ".jspx"]
 
-    def create_extractor(self) -> ElementExtractor:
+    def create_extractor(self) -> JavaElementExtractor:
         """Create a new element extractor instance."""
         return JavaElementExtractor()
 
