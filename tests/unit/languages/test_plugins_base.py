a0117d6b4912b299cf4649e2aee4c8dfa3df989d
aisheng.yu <aimasteracc@gmail.com>
2026-05-30 19:15:52 +0900

    test: delete 5 more duplicate aggregator files (all sources collected independently)

--- tests/unit/languages/test_plugins_base.py
+++
@@ -1,9 +0,0 @@
-"""Re-export aggregator for split plugins_base test modules.
-
-Originally a single 845-line file, now split into:
-- test_plugins_base_core.py: TestLanguagePlugin, TestElementExtractor, TestDefaultExtractorTraversalDirect, TestDefaultExtractorIntegration
-- test_plugins_base_advanced.py: TestLanguagePluginIntegration, TestEdgeCases, TestDefaultExtractorTraversal, TestDefaultExtractorErrorPaths
-"""
-
-from tests.unit.languages.test_plugins_base_advanced import *  # noqa: F401,F403
-from tests.unit.languages.test_plugins_base_core import *  # noqa: F401,F403
